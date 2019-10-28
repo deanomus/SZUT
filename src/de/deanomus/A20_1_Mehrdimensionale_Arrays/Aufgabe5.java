d62ded1d49ac461ad9b6de7c015d9e3768892555
Dean Reinhardt <d_reinhardt02@hotmail.com>
2019-10-28 15:02:43 +0100

    aufgabe 5 step 2

--- src/de/deanomus/A20_1_Mehrdimensionale_Arrays/Aufgabe5.java
+++ src/de/deanomus/A20_1_Mehrdimensionale_Arrays/Aufgabe5.java
@@ -10,6 +10,64 @@ public class Aufgabe5 {
 
     public static void aufg() {
 
+
+        Integer[][] data = {
+                {4, 7, 2, 45},
+                {8, 3, 21, 3},
+                {56, 2, 8, 9},
+                {32, 76, 22, 3}
+    };
+
+
+
+        for ( int zeile=0; zeile < data.length; zeile++) {
+            for ( int spalte = 0; spalte < data[zeile].length; spalte++) {
+                int summeNachbarn = 0;
+
+                // Linker Nachbar
+//              | 0 | 0 | 0 |
+//              | 1 | X | 0 |
+//              | 0 | 0 | 0 |
+                if(spalte>0) {
+                    summeNachbarn += data[zeile][spalte-1];
+                }
+                //oberer Linker Nachbar
+//              | 1 | 0 | 0 |
+//              | 0 | X | 0 |
+//              | 0 | 0 | 0 |
+                if(spalte>0 && zeile>0) {
+                    summeNachbarn += data[zeile-1][spalte-1];
+                }
+                //oben
+                if(zeile>0) {
+                    summeNachbarn += data[zeile-1][spalte];
+                }
+                //oberer Rechter Nachbar
+                if(zeile>0 && zeile<data[zeile].length-1) {
+                    summeNachbarn += data[zeile-1][spalte+1];
+                }
+                //rechter Nachbar
+
+
+
+            }
+        }
+
+
+
+
+
+
+        // -> Ausgabe
+        for ( int zeile=0; zeile < data.length; zeile++)
+        {
+            System.out.println("");
+            for ( int spalte = 0; spalte < data[zeile].length; spalte++)
+            {
+                System.out.print(data[zeile][spalte] + " | ");
+            }
+        }
+
     }
 
 
--- src/de/deanomus/core/Core.java
+++ src/de/deanomus/core/Core.java
@@ -82,8 +82,6 @@ public class Core {
         if(kate== 1 && aufg == 9) Aufgabe9.aufg();
         if(kate== 1 && aufg == 10) Aufgabe10.aufg();
         if(kate== 1 && aufg == 12) Aufgabe12.aufg();
-
-
         //Kategorie 2 -> Aufgaben
         if(kate== 2 && aufg == 0) de.deanomus.A15_1_ArbeitsauftragArrays.ArrayEinfuehrung.aufg();
         if(kate== 2 && aufg == 1) de.deanomus.A15_1_ArbeitsauftragArrays.Aufgabe1.aufg();
@@ -92,18 +90,13 @@ public class Core {
         if(kate== 2 && aufg == 4) de.deanomus.A15_1_ArbeitsauftragArrays.Aufgabe4.aufg();
         if(kate== 2 && aufg == 5) de.deanomus.A15_1_ArbeitsauftragArrays.Aufgabe5.aufg();
         if(kate== 2 && aufg == 6) de.deanomus.A15_1_ArbeitsauftragArrays.Aufgabe6.aufg();
-
-
-
-
         //Kategorie 3 -> Aufgaben
         if(kate == 3 && aufg == 1) de.deanomus.A20_1_Mehrdimensionale_Arrays.Aufgabe1.aufg();
         if(kate == 3 && aufg == 2) de.deanomus.A20_1_Mehrdimensionale_Arrays.Aufgabe2.aufg();
         if(kate == 3 && aufg == 3) de.deanomus.A20_1_Mehrdimensionale_Arrays.Aufgabe3.aufg();
         if(kate == 3 && aufg == 4) de.deanomus.A20_1_Mehrdimensionale_Arrays.Aufgabe4.aufg();
         if(kate == 3 && aufg == 5) de.deanomus.A20_1_Mehrdimensionale_Arrays.Aufgabe5.aufg();
-
-
+        // -> Exit
         if(kate== 0 && aufg == 0) return;
 
 
