c3a8f9c4cd2695f400439db453bc8dd886f3dbab
Dean Reinhardt <d_reinhardt02@hotmail.com>
2019-10-29 09:06:35 +0100

    int scan convert now from text to int, like eins to 1 or two to 2

--- src/de/deanomus/util/Data.java
+++ src/de/deanomus/util/Data.java
@@ -11,6 +11,7 @@ public class Data {
             i = Integer.parseInt(s);
         }
         catch(Exception e) {
+            if(possible2Convert(s)) return text2int(s);
             System.out.print("\n\nEin Fehler ist aufgetreten! Sicher das du eine Zahl eingegeben hast?\n");
         }
 
@@ -19,6 +20,49 @@ public class Data {
         }
         return -1;
 
+    }
+
+    public static boolean possible2Convert(String number) {
+        if(!(text2int(number) == null)) return true;
+        return false;
+    }
+
+    public static Integer text2int(String number) {
+        //Deutsch / German
+        if(number.equalsIgnoreCase("null")) return 0;
+        if(number.equalsIgnoreCase("eins")) return 1;
+        if(number.equalsIgnoreCase("zwei")) return 2;
+        if(number.equalsIgnoreCase("drei")) return 3;
+        if(number.equalsIgnoreCase("vier")) return 4;
+        if(number.equalsIgnoreCase("fünf")) return 5;
+        if(number.equalsIgnoreCase("sechs")) return 6;
+        if(number.equalsIgnoreCase("sieben")) return 7;
+        if(number.equalsIgnoreCase("acht")) return 8;
+        if(number.equalsIgnoreCase("neun")) return 9;
+        if(number.equalsIgnoreCase("zehn")) return 10;
+        if(number.equalsIgnoreCase("elf")) return 11;
+        if(number.equalsIgnoreCase("zwölf")) return 12;
+        if(number.equalsIgnoreCase("dreizehn")) return 13;
+        if(number.equalsIgnoreCase("vierzehn")) return 14;
+
+        //English
+        if(number.equalsIgnoreCase("one")) return 1;
+        if(number.equalsIgnoreCase("two")) return 2;
+        if(number.equalsIgnoreCase("three")) return 3;
+        if(number.equalsIgnoreCase("four")) return 4;
+        if(number.equalsIgnoreCase("five")) return 5;
+        if(number.equalsIgnoreCase("six")) return 6;
+        if(number.equalsIgnoreCase("eleven")) return 7;
+        if(number.equalsIgnoreCase("ate")) return 8;
+        if(number.equalsIgnoreCase("nine")) return 9;
+        if(number.equalsIgnoreCase("ten")) return 10;
+//        if(number.equalsIgnoreCase("")) return 11;
+        if(number.equalsIgnoreCase("twelve")) return 12;
+
+
+
+
+        return null;
     }
 
 
