66f4e9a63acc7e9d6477b5fe1e00f4c2db8f1e5b
Dean Reinhardt <d_reinhardt02@hotmail.com>
2019-12-10 10:14:11 +0100

    fussballmanager branch and torwart (2. worksheet)

---
+++ src/de/deanomus/OGG/fussball2/Torwart.java
@@ -0,0 +1,7 @@
+package de.deanomus.OGG.fussball2;
+
+public class Torwart extends Player {
+    public Torwart(String name, int age, int experience, int motivation, int score) {
+        super(name, age, experience, motivation, score);
+    }
+}
