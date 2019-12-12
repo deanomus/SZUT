7d019270a5808fafcd40cbcbfdecf587eb55113b
Dean Reinhardt <d_reinhardt02@hotmail.com>
2019-12-12 12:24:45 +0100

    fussball3 worksheet finished

---
+++ src/de/deanomus/OGG/fussball3/Person.java
@@ -0,0 +1,14 @@
+package de.deanomus.OGG.fussball3;
+
+public abstract class Person {
+
+    protected String
+                name;
+    protected int
+                age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
