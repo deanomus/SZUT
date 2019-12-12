57b28dc3aa9d074d315467e97e442517af70e54f
Dean Reinhardt <d_reinhardt02@hotmail.com>
2019-12-12 12:08:31 +0100

    copied fussball2 to fussball3 for worksheet 3

--- src/de/deanomus/OGG/FussballVersion.java
+++ src/de/deanomus/OGG/FussballVersion.java
@@ -10,13 +10,15 @@ public class FussballVersion {
     static Scanner scan = Core.scan;
 
     public static void start() {
-        System.out.println("Which version to start? (v1, v2)");
+        System.out.println("Which version to start? (v1, v2, v3)");
 
         int v = Data.intScan(scan);
         if(v == 1) {
             de.deanomus.OGG.fussball1.Programm.start();
         } else if(v == 2) {
             de.deanomus.OGG.fussball2.Programm.start();
+        } else if(v == 3) {
+            de.deanomus.OGG.fussball3.Programm.start();
         }
     }
 
